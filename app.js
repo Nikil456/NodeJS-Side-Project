838ee819f5338ef3ba31d283c98e44a494870414
Nikil Kandala <kandala.nikil@gmail.com>
2025-01-20 21:23:04 -0500

    Changed data management to Mongoose library.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+//const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,9 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('678070534d8dc51fd93787fc')
+  User.findById('678ef580f68bf77a4b1cf2c9')
     .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
+      req.user = user;
       next();
     })
     .catch(err => console.log(err));
@@ -32,6 +33,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose
+  .connect('mongodb+srv://kandalanikil:AfemlUlwvAybOAin@cluster0.s3y3s.mongodb.net/shop?retryWrites=true')
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Nikil',
+          email: 'kandala@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    }); 
     app.listen(3000);
-});
+  })
+  .catch(err => {
+    console.log(err);
+  });
